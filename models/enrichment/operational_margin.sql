6cf992cea746a5d23444b3f88157e6a0c3ca4441
Bradley Aidan Johnson <bradleyaidanjohnson@gmail.com>
2024-04-17 04:48:54 +0000

    added a materialised finance days table

---
+++ models/enrichment/operational_margin.sql
@@ -0,0 +1,23 @@
+WITH agg_orders AS(
+    SELECT orders_id
+        , date_date
+        , ROUND(SUM(revenue), 2) AS revenue
+        , SUM(quantity) AS quantity
+        , ROUND(SUM(purchase_cost), 2) AS purchase_cost
+        , ROUND(SUM(margin), 2) AS margin
+    FROM {{ ref("margin_enr")}}
+    GROUP BY orders_id, date_date
+)
+SELECT agg_orders.orders_id
+    , date_date
+    , revenue
+    , quantity
+    , purchase_cost
+    , margin
+    , shipping_fee
+    , logcost
+    , ship_cost
+    , ROUND((revenue + shipping_fee) - (logcost + ship_cost), 2) AS op_margin
+FROM agg_orders
+JOIN {{ ref("stg_raw__ship")}} ship
+ON agg_orders.orders_id = ship.orders_id
--- models/mart/finance_days_b.sql
+++ models/mart/finance_days_b.sql
@@ -3,7 +3,7 @@ SELECT date_date
     , ROUND(SUM(revenue), 0) AS revenue
     , ROUND(AVG(revenue), 1) AS average_basket
     , ROUND(SUM(margin), 0) AS margin
-    , ROUND(SUM(operational_margin), 0) AS operational_margin
-FROM {{ ref("int_orders_operational")}}
+    , ROUND(SUM(op_margin), 0) AS op_margin
+FROM {{ ref("operational_margin")}}
 GROUP BY date_date
 ORDER BY date_date DESC
